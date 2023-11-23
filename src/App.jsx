1e275412c61f7661b18c9ccc371f4a5eab84714f
rehaddev <rehadhasanshawn2@gmail.com>
2023-11-23 12:10:01 +0600

    class-2 done

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,32 @@
-import React, {useState} from 'react';
+import HomePage from "./pages/HomePage.jsx";
+import DocsPage from "./pages/DocsPage.jsx";
+import BlogPage from "./pages/BlogPage.jsx";
+
+import {
+    createRoutesFromElements,
+    createBrowserRouter,
+    RouterProvider,
+    Route,
+} from "react-router-dom";
+import RouteLayout from "./components/RouteLayout.jsx";
+
 
 const App = () => {
-    const [city, setCity] = useState(['Dhaka','Rangpur','Chittagong','Sylhet','Pabna']);
+    const router = createBrowserRouter(
+        createRoutesFromElements(
+            <Route path="/" element={<RouteLayout/>}>
+                <Route path="/" element={<HomePage/>}/>
+                <Route path="/docs" element={<DocsPage/>}/>
+                <Route path="/blog" element={<BlogPage/>}/>
+            </Route>
+        )
+    );
     return (
-        <div>
-            <ul>
-                {
-                    city.map((item,i)=>{
-                        return <li>{item}</li>
-                    })
-                }
-            </ul>
-        </div>
+        <>
+            <RouterProvider
+                router={router}
+            />
+        </>
     );
 };
 
